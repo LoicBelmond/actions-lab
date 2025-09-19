7d633ab8d56f916cc4a6249cd3a104593112d9d2
LoicBelmond <Loicbelmondnz@gmail.com>
2025-09-19 14:59:48 -0400

    Fix Prettier formatting and remove stray workflow file

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,19 +1,21 @@
+// eslint.config.cjs
 const js = require('@eslint/js')
+const globals = require('globals')
 
 module.exports = [
   js.configs.recommended,
   {
     languageOptions: {
       ecmaVersion: 'latest',
-      sourceType: 'commonjs'
-    },
-    env: {
-      node: true,
-      jest: true
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
     },
     ignores: ['node_modules/', 'dist/', 'coverage/'],
     rules: {
-      // add custom rules here if your course requires any
-    }
-  }
+      // add custom rules here if needed
+    },
+  },
 ]
--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,6 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'json-summary'],
   coverageThreshold: {
-    global: { statements: 80, branches: 80, functions: 80, lines: 80 }
-  }
+    global: { statements: 80, branches: 80, functions: 80, lines: 80 },
+  },
 }
